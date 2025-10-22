4d40c74d228c2369de231c3ccf16e09d144cfe55
astraglus03 <kkd0250@gmail.com>
2025-10-22 20:35:39 +0900

    fix: 오타 제거

--- src/services/editorStompService.js
+++ src/services/editorStompService.js
@@ -182,7 +182,7 @@ export const disconnectStomp = () => {
   
   if (client) {
     try {
-      client.deㅇactivate();
+      client.deactivate();
       console.log('🔌 STOMP 연결 해제 완료');
     } catch (error) {
       console.error('Error disconnecting STOMP:', error);
