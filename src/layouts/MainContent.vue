3fd7e3174e777ff2027928b5daf0ed5ba172d261
JihyeonKim <wwwlllwwwlll92@gmail.com>
2025-10-27 15:43:40 +0900

    fix: 채널변경 수정

--- src/layouts/MainContent.vue
+++ src/layouts/MainContent.vue
@@ -137,7 +137,7 @@ const contentStyle = computed(() => {
       :selected-channel="selectedChannel"
       :navigate-to-personal-drive="navigateToPersonalDrive"
     />
-    
+
     <!-- 멤버 사이드바 (프로젝트 워크스페이스일 때만 표시) -->
     <MemberSidebar
       v-if="workspaceType === 'project' && memberSidebarVisible"
